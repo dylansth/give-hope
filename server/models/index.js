462fa14319c127163f0f2cdc0292751330579609
Mona Lisa <130613163+rudrijoshi@users.noreply.github.com>
2023-07-12 14:13:25 -0400

    created models folder and added text to index.js

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Campaign = require('./campaign');
+const Donation = require('./donation');
+const Purchase_power = require('./purchase_power');
+const Review = require('./review');
+const User = require('./user');
+
+module.exports = { Campaign, Donation, Purchase_power, Review, User }
