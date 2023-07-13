a8cced612872183f065a5ae051cfcccb4163589a
Mona Lisa <130613163+rudrijoshi@users.noreply.github.com>
2023-07-13 10:48:52 -0400

    added text to review model

---
+++ server/models/review.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+const Campaign = require('./campaign');
+
+const User = require('./user');
+
+
+const reviewSchema = new Schema({
+    description: {
+        type: String,
+    },
+    creatorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    campaignId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Campaign',
+    }
+});
+
+const Review = model('Review', reviewSchema);
+
+module.exports = Review;
