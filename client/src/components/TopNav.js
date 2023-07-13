70a43f948c08882f1dbae13434447e6dd081ad0a
D <dylan@Ds-MacBook-Air.local>
2023-07-13 16:05:28 -0700

    minor topnav styling changes

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,52 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 function TopNav() {
-    const logout = (event) => {
-        event.preventDefault();
-        // Auth.logout();
-        // For Auth helper & JWT integration.
-      };
+
     return (
-        <div>
-            <navbar className="">
-                <div>
-                    <Link className="" to="/">
-                        <p className="" style={{ fontSize: '1.5rem' }}>
-                        Home
-                        </p>
-                    </Link>
-                </div>
-                <div>
-                    <Link className="" to="/explore">
-                        <p className="" style={{ fontSize: '1.5rem' }}>
-                            Explore
-                        </p>
-                    </Link>
-                </div>
-                <div>
-                    <Link className="" to="/sign-in">
-                        <button className="" onClick="">
-                        Sign In
-                        </button>
-                    </Link>
-                </div>
-                <div>
-                    <Link className="" to="/">
-                    <button className="" onClick={logout}>
-                        Log Out
-                        </button>
-                    </Link>
-                </div>
-                <div>
-                    <Link className="" to="/sign-up">
-                    <button className="" onClick="">
-                        Sign Up
-                        </button>
-                    </Link>
-                </div>
-            </navbar>
-        </div>
+
+                <Container>
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            <Link className="" to="/">
+                                <p className="" style={{ fontSize: '1.5rem' }}>
+                                    Home
+                                </p>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="" to="/explore">
+                                <p className="" style={{ fontSize: '1.5rem' }}>
+                                    Explore
+                                </p>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="" to="/sign-in">
+                                <button className="" onClick="">
+                                    Sign In
+                                </button>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="" to="/">
+                                <button className="">
+                                    Log Out
+                                </button>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="" to="/sign-up">
+                                <button className="">
+                                    Sign Up
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
+                </Container>
+                
+
     )
 }
 
