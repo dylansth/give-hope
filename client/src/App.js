d336314111ef91bdc763a49916c0a8cd19cc146b
D <dylan@Ds-MacBook-Air.local>
2023-07-14 11:55:43 -0700

    package-lock, initial apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import TopNav from './components/TopNav';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,6 +17,27 @@ import Explore from './pages/Explore';
 import Fundraiser from './pages/Fundraiser';
 import CreateFundraiser from './pages/CreateFundraiser';
 import NotFound from './pages/NotFound';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
@@ -39,67 +68,68 @@ function App() {
       </div> */}
 
       <div>
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
-            <Route
-              path="/sign-in"
-              element={<SignIn />}
-            />
-            <Route
-              path="/sign-up"
-              element={<SignUp />}
-            />
-            <Route
-              path="/explore"
-              element={<Explore />}
-            />
-            <Route
-              path="/fundraiser/:id"
-              element={<Fundraiser />}
-            />
-            <Route
-              path="/create-fundraiser"
-              element={<CreateFundraiser />}
-            />
-            <Route
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes>
-
-          {/* Rendering Everything For Development */}
-          <div>
-            <TopNav />
-          </div>
-          <div>
-            <Home />
-          </div>
-          <div>
-            <SignIn />
-          </div>
-          <div>
-            <SignUp />
-          </div>
-          <div>
-            <Explore />
-          </div>
-          <div>
-            <Fundraiser />
-          </div>
-          <div>
-            <CreateFundraiser />
-          </div>
-          <div>
-            <Footer />
-          </div>
-          <div>
-            <NotFound />
-          </div>
-        </Router>
+        <ApolloProvider client={client}>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/sign-in"
+                element={<SignIn />}
+              />
+              <Route
+                path="/sign-up"
+                element={<SignUp />}
+              />
+              <Route
+                path="/explore"
+                element={<Explore />}
+              />
+              <Route
+                path="/fundraiser/:id"
+                element={<Fundraiser />}
+              />
+              <Route
+                path="/create-fundraiser"
+                element={<CreateFundraiser />}
+              />
+              <Route
+                path="*"
+                element={<NotFound />}
+              />
+            </Routes>
+            {/* Rendering Everything For Development */}
+            <div>
+              <TopNav />
+            </div>
+            <div>
+              <Home />
+            </div>
+            <div>
+              <SignIn />
+            </div>
+            <div>
+              <SignUp />
+            </div>
+            <div>
+              <Explore />
+            </div>
+            <div>
+              <Fundraiser />
+            </div>
+            <div>
+              <CreateFundraiser />
+            </div>
+            <div>
+              <Footer />
+            </div>
+            <div>
+              <NotFound />
+            </div>
+          </Router>
+        </ApolloProvider>
       </div>
     </>
   );
