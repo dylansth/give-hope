ff03638a459b0bf304a61682becd42560a3fa648
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-14 10:48:18 -0400

    mutations public side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -43,13 +43,60 @@ mutation CreateCampaign($campaignData: CampaignInput!) {
 
 
 
-export const REMOVE_BOOK = gql`
+export const UPDATE_CAMPAIGN = gql`
 mutation UpdateCampaign($id: ID!, $campaignData: updateCampaignInput!) {
   updateCampaign(_id: $id, campaignData: $campaignData) {
     description
     title
     _id
+    endDate
+    targetAmount
   }
 }
 `;
 
+export const DELETE_CAMPAIGN = gql`
+mutation Mutation($campaignId: ID!) {
+  deleteCampaign(campaignId: $campaignId) {
+    creatorId {
+      _id
+    }
+  }
+}
+`;
+
+export const MAKE_DONATION = gql`
+mutation Mutation($campaignId: ID!, $amount: Int!) {
+  makeDonation(campaignId: $campaignId, amount: $amount) {
+    amount
+    createdAt
+    donorId {
+      _id
+    }
+    campaignId {
+      _id
+    }
+  }
+}
+`;
+
+export const ADD_REVIEW = gql`
+mutation Mutation($campaignId: ID!, $description: String!) {
+  createReview(campaignId: $campaignId, description: $description) {
+    description
+    campaigns {
+      _id
+    }
+  }
+}
+`;
+
+export const DELETE_REVIEW = gql`
+mutation DeleteReview($reviewId: ID!) {
+  deleteReview(reviewId: $reviewId) {
+    creatorId {
+      _id
+    }
+  }
+}
+`;
