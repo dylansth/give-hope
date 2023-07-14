927b2d69580e0b74c9119a7fecb63d21022bd706
Mona Lisa <130613163+rudrijoshi@users.noreply.github.com>
2023-07-14 13:56:39 -0400

    upd

--- client/src/pages/CreateFundraiser.js
+++ client/src/pages/CreateFundraiser.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useMutation } from 'apollo-client';
+import { useMutation } from '@apollo/client';
 import { CREATE_CAMPAIGN } from '../utils/mutations';
 
 const CampaignForm = () => {
