d4fe63b9d3f1a5e9ddaa02deaccadb02fb09f233
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-15 10:44:45 -0400

    format date applied

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -7,38 +7,38 @@ import '../styles/style.css'
 function Explore() {
     const { loading, data } = useQuery(QUERY_CAMPAIGN
       );
-    
       const campaignList = data?.campaigns || [];
-
-    return(
+   
+      return (
         <div className="flex flex-col">
-        <div className="flex  flex-wrap -m-4">
-          {campaignList.map((campaign) => (
-            <div className="sm:w-1/4 w-full p-4 relative" key={campaign.title}>
-              <div className="relative bg-slate-400">
-                <img
-                  alt="gallery"
-                  className="w-100 h-100 object-cover object-center"
-                  src={campaign.image}
-                />
-                 </div>
-                <div className="absolute inset-0 flex items-center justify-center opacity-5 hover:opacity-100 z-10">
-                  <div className="px-8 py-10 bg-gray-600 bg-opacity-80 text-white ">
-                    <h1 className="text-white title-font text-lg font-medium mb-3">
-                      {campaign.title}
-                    </h1>
-                    <p className="text-white object-cover leading-relaxed">{campaign.description}</p>
-
-                    <div className='flex flex-row justify-center flex-wrap' >
+          <div className="flex flex-wrap -m-4">
+            {campaignList.map((campaign) => {
+              //  const endDate = new Date(campaign.endDate);
+              //  const formattedEndDate = endDate.toLocaleTimeString('en-US'); 
+             
+              return (
+                <div className="sm:w-1/4 w-full p-4 relative" key={campaign.title}>
+                  <div className="relative bg-slate-400">
+                    <img
+                      alt="gallery"
+                      className="w-80 h-60 object-cover object-center"
+                      src={campaign.image}
+                    />
+                    <div className="absolute bottom-0 left-0 right-0 px-8 py-10 bg-gray-600 bg-opacity-80 text-white">
+                      <h1 className="text-white title-font text-lg font-medium mb-3">
+                        {campaign.title}
+                      </h1>
+                      <p className="text-white leading-relaxed">
+                        Target Amount: ${campaign.targetAmount} | End Date: {campaign.endDate}
+                      </p>
                     </div>
                   </div>
                 </div>
-              </div>
-           
-          ))}
+              );
+            })}
+          </div>
         </div>
-      </div>
-    )
-}
-
+      );
+    
+            }
 export default Explore;
--- server/models/campaign.js
+++ server/models/campaign.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 const Review = require('./review');
 
 const campaignSchema = new Schema({
@@ -26,6 +27,7 @@ const campaignSchema = new Schema({
     },
     endDate: {
         type: Date,
+        get: (timestamp) => dateFormat(timestamp),
     },
     donations: [{
         type: Schema.Types.ObjectId,
@@ -33,6 +35,8 @@ const campaignSchema = new Schema({
     }],
     createdAt: {
         type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
     },
     reviews: [{
         type: Schema.Types.ObjectId,
