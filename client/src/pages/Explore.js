3bd0f4b0356eb31d0f92d72f5a074760a2512676
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-16 10:11:47 -0400

    conditional rendering by amount

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -42,65 +42,65 @@ function Explore() {
           // Format Money
           const moneyformatted = USDollar.format(campaign.targetAmount);
           // Format timer
-          const milliseconds = campaign.endDate
+          const milliseconds = campaign.endDate;
           const dateString = millisecondsToDateString(milliseconds);
-          console.log(dateString)
-          console.log(campaign.currentAmount)
-
-            // Hover progress bar and see the amount 
+  
+          // Hover progress bar and see the amount 
           const tooltip = (
             <Tooltip id="tooltip">
               Current Amount: {`$${campaign.currentAmount}`}
             </Tooltip>
           );
-
-          // Format % for progress bar
-
+  
+          // Format % and color  progress bar
           const percentageDifference = (campaign.currentAmount / campaign.targetAmount) * 100;
-
-            let variant = 'success';
-
-            if (percentageDifference >= 65) {
-              variant = 'success';
-            } else if (percentageDifference >= 40) {
-              variant = 'warning';
-            } else {
-              variant = 'danger';
-            }
-
+  
+          let variant = 'success';
+  
+          if (percentageDifference >= 65) {
+            variant = 'success';
+          } else if (percentageDifference >= 40) {
+            variant = 'warning';
+          } else {
+            variant = 'danger';
+          }
+  
+          // conditional rendering
+          const today = Date.now();
+          const isCampaignDisplaying  = campaign.currentAmount < campaign.targetAmount;
+      
           return (
-            <div className="sm:w-1/4 w-1/4  p-4 m-5 border-solid border-2 border-indigo-600" key={campaign.title}>
-              <div className="h-80 relative bg-slate-400">
-                <img
-                  alt="gallery"
-                  className="w-full h-full object-cover object-center"
-                  src={campaign.image}
-                />
-              </div>
-              <div className="bottom-0 left-0 right-0  bg-gray-600 text-white p-0">
-                <h1 className="text-white title-font text-lg font-medium mb-3 text-center">
-                  {campaign.title}
-                </h1>
-                <p className="text-white leading-relaxed text-center">
-                  🥅 Target Amount: {moneyformatted}
-                </p>
-              <div className='px-5 pb-2'>
-                <OverlayTrigger placement="top" overlay={tooltip}>
-                <ProgressBar animated={true} max={campaign.targetAmount} now={campaign.currentAmount} label={`$${campaign.currentAmount}`} variant={variant} striped={true} /> 
-                </OverlayTrigger>
+            (isCampaignDisplaying) && (
+              <div className="sm:w-1/4 w-1/4 p-4 m-5 border-solid border-2 border-indigo-600" key={campaign.title}>
+                <div className="h-80 relative bg-slate-400">
+                  <img
+                    alt="gallery"
+                    className="w-full h-full object-cover object-center"
+                    src={campaign.image}
+                  />
                 </div>
-                <div className="text-white leading-relaxed text-center">
-                  <p>⌛End:<Countdown dateString={dateString} /></p>
+                <div className="bottom-0 left-0 right-0 bg-gray-600 text-white p-0">
+                  <h1 className="text-white title-font text-lg font-medium mb-3 text-center">
+                    {campaign.title}
+                  </h1>
+                  <p className="text-white leading-relaxed text-center">
+                    🥅 Target Amount: {moneyformatted}
+                  </p>
+                  <div className="px-5 pb-2">
+                    <OverlayTrigger placement="top" overlay={tooltip}>
+                      <ProgressBar animated={true} max={campaign.targetAmount} now={campaign.currentAmount} label={`$${campaign.currentAmount}`} variant={variant} striped={true} />
+                    </OverlayTrigger>
+                  </div>
+                  <div className="text-white leading-relaxed text-center">
+                    <p>⌛End:<Countdown dateString={dateString} /></p>
+                  </div>
                 </div>
-
-
               </div>
-            </div>
+            )
           );
         })}
       </div>
     </div>
   );
-
 }
 export default Explore;
