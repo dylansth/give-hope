3d32be80ce25cac2b50000a74f940d7f2d977d46
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-16 10:45:29 -0400

    inital style createFundraiser. Img upload adding front end

--- client/src/pages/CreateFundraiser.js
+++ client/src/pages/CreateFundraiser.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { CREATE_CAMPAIGN } from '../utils/mutations';
 
+
+
 const CampaignForm = () => {
     const [formData, setFormData] = useState({
         title: '',
@@ -60,45 +62,90 @@ const CampaignForm = () => {
     };
 
 
+    // Handle image
+    const [image, setImage] = useState("");
+
+    function convertToBase64(e) {
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+    
+      reader.onload = () => {
+        console.log(reader.result); // string to save in MongoDB
+        setImage(reader.result);
+      };
+    
+      reader.onerror = (error) => {
+        console.log("Error:", error);
+      };
+    }
+
     return (
-        <div>
-            <h1>Create Campaign</h1>
-            <form onSubmit={handleFormSubmit}>
-                <label>Title:</label>
-                <input
-                    type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleInputChange}
-                />
-
-                <label>Description:</label>
-                <input
-                    type="text"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleInputChange}
-                />
-
-                <label>endDate:</label>
-                <input
-                    type="text"
-                    name="endDate"
-                    value={formData.endDate}
-                    onChange={handleInputChange}
-                />
-                <label>Target Amount:</label>
-                <input
-                    type="text"
-                    name="targetAmount"
-                    defaultValue={formData.targetAmount}
-                    onChange={handleInputChange}
-                />
-                {/* Add more input fields for other campaign fields */}
-
-                <button type="submit">Create Campaign</button>
-            </form>
-        </div>
+        <div className="flex flex-col items-center mt-8">
+        <h1 className="text-2xl font-bold mb-4">Create Campaign</h1>
+        <form onSubmit={handleFormSubmit} className="w-full max-w-md">
+          <div className="mb-4">
+            <label className="block font-semibold mb-1" htmlFor="title">Title:</label>
+            <input
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold mb-1" htmlFor="description">Description:</label>
+            <input
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold mb-1" htmlFor="endDate">End Date:</label>
+            <input
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              type="text"
+              name="endDate"
+              value={formData.endDate}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold mb-1" htmlFor="targetAmount">Target Amount:</label>
+            <input
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              type="text"
+              name="targetAmount"
+              defaultValue={formData.targetAmount}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold mb-1" htmlFor="image">Upload your image:</label>
+            <input
+              accept="image/*"
+              type="file"
+              name="image"
+              defaultValue={formData.image}
+              onChange={convertToBase64}
+              className="mb-2"
+            />
+            {image !== "" && (
+              <img src={image} alt="Uploaded" className="w-24 h-24 rounded-md" />
+            )}
+          </div>
+          {/* Add more input fields for other campaign fields */}
+          <button
+            type="submit"
+            className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600"
+          >
+            Create Campaign
+          </button>
+        </form>
+      </div>
     );
 };
 
