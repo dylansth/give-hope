132bc367d996e7bd5d5bb15789c9bd7c94dab85e
D <dylan@Ds-MacBook-Air.local>
2023-07-17 12:39:31 -0700

    home.js, conditional rendering logic

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_GET_ME } from '../utils/queries';
+
 import '../styles/style.css'
 
 function Home() {
- 
-  return (
-    <div className="home-container">
-    
-    </div>
-  );
+    const { data, loading } = useQuery(QUERY_GET_ME);
+    const username = data?.me?.username || [];
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+    return (
+
+        <div>
+            {Auth.loggedIn() ? (
+                <>
+                    <Card body>Welcome to GiveHope {username}</Card>
+                </>
+            ) : (
+                <>
+                    <Card body>Log In or Sign Up to Start a Fundraiser or Begin Donating</Card>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default Home;
