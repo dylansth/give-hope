be4e75fdc31b62d1a38d3afca53ff9d0e0ebbdef
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-18 12:30:18 -0400

    heroku d

--- server/server.js
+++ server/server.js
@@ -39,38 +39,38 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.get('/', (req, res) => {
-  imgSchema.find({})
-  .then((data, err)=>{
-      if(err){
-          console.log(err);
-      }
-      res.render('imagepage',{items: data})
-  })
-});
+// app.get('/', (req, res) => {
+//   imgSchema.find({})
+//   .then((data, err)=>{
+//       if(err){
+//           console.log(err);
+//       }
+//       res.render('imagepage',{items: data})
+//   })
+// });
 
 
-app.post('/', upload.single('image'), (req, res, next) => {
+// app.post('/', upload.single('image'), (req, res, next) => {
 
-  var obj = {
-      name: req.body.name,
-      desc: req.body.desc,
-      image: {
-          data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-          contentType: 'image/png'
-      }
-  }
-  imgSchema.create(obj)
-  .then ((err, item) => {
-      if (err) {
-          console.log(err);
-      }
-      else {
-          // item.save();
-          res.redirect('/');
-      }
-  });
-});
+//   var obj = {
+//       name: req.body.name,
+//       desc: req.body.desc,
+//       image: {
+//           data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+//           contentType: 'image/png'
+//       }
+//   }
+//   imgSchema.create(obj)
+//   .then ((err, item) => {
+//       if (err) {
+//           console.log(err);
+//       }
+//       else {
+//           // item.save();
+//           res.redirect('/');
+//       }
+//   });
+// });
 
 
 ////
