7b3549bc61432fb8fcc2643925dd8cfdedaf9aef
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-18 12:20:49 -0400

    new depl

--- client/src/index.js
+++ client/src/index.js
@@ -20,15 +20,15 @@ reportWebVitals();
 
 // For the install to work, a service worker must be successfully registered with scope
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service worker registered:', registration);
-      })
-      .catch(error => {
-        console.log('Service worker registration failed:', error);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => {
+//         console.log('Service worker registered:', registration);
+//       })
+//       .catch(error => {
+//         console.log('Service worker registration failed:', error);
+//       });
+//   });
+// }
 
