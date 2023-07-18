0df676c678616c46fff4e4f01df16b069731dfe7
D <dylan@Ds-MacBook-Air.local>
2023-07-18 00:47:06 -0700

    home styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_GET_ME } from '../utils/queries';
 import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
+import HomeSVG from '../assets/home-picture.svg'
 import '../styles/style.css'
 
 function Home() {
@@ -14,26 +14,35 @@ function Home() {
         return <div>Loading...</div>
     }
     return (
-
-        <Container fluid>
+        <div className=''>
             <h1 class="my-6 ml-6">
                 GiveHope
             </h1>
             {Auth.loggedIn() ? (
                 <div>
-                        <p>
-                            Welcome, {username}
-                        </p>
+                    <p>
+                        Welcome, {username}
+                    </p>
                 </div>
             ) : (
                 <div className="mt-4">
-                        <p>
-                            Welcome! Please <Link to="/sign-in">Sign In</Link> Or{' '}
-                            <Link to="/sign-up">Sign Up</Link> to Donate or Create a Fundraiser.
-                        </p>
+                    <p>
+                        Welcome! Please <Link to="/sign-in">Sign In</Link> Or{' '}
+                        <Link to="/sign-up">Sign Up</Link> to Donate or Create a Fundraiser.
+                    </p>
                 </div>
             )}
-        </Container>
+
+            <div class="h-60 flex flex-col justify-end border-y-2 border-y-black" style={{ backgroundImage: `url(${HomeSVG})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                {/* <img src="home-picture.svg" alt="" /> */}
+                <div className='bg-white/[.50] text-center'>
+                    <h2 className="font-bold text-3xl"> Every Donation Matters. </h2>
+                    <p className="font-medium text-lg">
+                        1% of your income can change someones life. 
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
